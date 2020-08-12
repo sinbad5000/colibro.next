5da843f28d01a373eb344331cd23996cb66a2599
Paul <pdpadian@gmail.com>
2020-08-12 16:59:00 -0600

    cover page done

--- pages/Regions/Antarctica.js
+++ pages/Regions/Antarctica.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-<<<<<<< HEAD
-export default function Home() {
-=======
 export default function Antarctica() {
->>>>>>> d46a330cb6b12f834276eb29610f7082ad8169e7
     return (
         <div>
             <Head>
