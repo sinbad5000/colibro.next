ac3a6c25020aea3910882ea2f34b30fc62dca04b
Paul <pdpadian@gmail.com>
2020-08-12 15:49:43 -0600

    throw commit

--- pages/Cover.js
+++ pages/Cover.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const mainStyle = style({ background: 'url(/public/jared-rice.png) no-repeat center center scroll' })
+export default function Cover() {
     return (
-        <div>
+        <div styl>
 
         </div>
     )
